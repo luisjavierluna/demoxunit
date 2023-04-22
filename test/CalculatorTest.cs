ecb24ea3b141e8ad0221913adb1aafe3a3148a13
luisjavierluna <javierlunaluis@gmail.com>
2023-04-22 17:57:16 -0600

    5. Creando tu primera prueba unitaria

---
+++ test/CalculatorTest.cs
@@ -0,0 +1,30 @@
+using Xunit;
+
+public class CalculatorTest
+{
+    [Fact]
+    public void Sum()
+    {
+        // Arrange
+        var calculator = new Calculator();
+
+        //Act
+        var result = calculator.Sum(2, 3);
+
+        //Assert
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Sum_0_4()
+    {
+        // Arrange
+        var calculator = new Calculator();
+
+        //Act
+        var result = calculator.Sum(0, 4);
+
+        //Assert
+        Assert.Equal(6, result);
+    }
+}
