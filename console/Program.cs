24e8c234876454350a8ab09e7504f47648368793
luisjavierluna <javierlunaluis@gmail.com>
2023-04-23 20:19:45 -0600

    8. Configurando proyecto base

--- console/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ingrese un número");
-int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese un número");
-int num2 = int.Parse(Console.ReadLine());
-var calculator = new Calculator();
-Console.WriteLine($"Total: {calculator.Sum(num1, num2)}");
-
